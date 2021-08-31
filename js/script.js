ae41558ccb2a83c5e1ff1231318e74a3ca9ff64f
cwhyman <cwhyma@gmail.com>
2021-08-31 20:33:55 +0100

    Added values for quantum type (e.g. number of units), and a function to update the table with the quantum type of the selected land use class

--- js/script.js
+++ js/script.js
@@ -1,56 +1,35 @@
 'use strict';
 
 //define an object for the land use classes/sub-classes, to use in the cascading dropdowns
-/* const landUseDefinitions = {
-    'A1': ['food retail above 100sqm', 'non-food retail above 100sqm'],
-    'A2-A5': ['financial / professional services; cafes & restaurants; drinking establishments; take-aways above 100 sqm'],
-    'B1': ['business offices'],
-    'B1': ['light industry and research and development'],
-    'B2-B8': ['general industrial, storage or distribution'],
-    'C1': ['hotels'],
-    'C2': ['hospitals'],
-    'C2': ['care homes / secure accommodation'],
-    'C3-C4': ['dwellings (all)'],
-    'D1': ['nurseries'],
-    'D1': ['primary schools / secondary schools/ sixth form colleges'],
-    'D1': ['universities and colleges'],
-    'D1': ['health centre, including dentists'],
-    'D1': ['other (e.g. library, church, etc.)'],
-    'D2': ['sports (e.g. sports hall, swimming, gymnasium, etc.)'],
-    'D2': ['other (e.g. cinema, bingo, etc.)'],
-    'Student accomodation': ['Student accomodation'],
-    'Older persons housing': ['Older persons housing'],
-} */
-
-const newLandUseDefinitions = [
-   {landUse:'A1', subLandUse:'food retail above 100sqm'},
-   {landUse:'A1', subLandUse:'non-food retail above 100sqm'},
-   {landUse:'A2-A5', subLandUse:'financial / professional services; cafes & restaurants; drinking establishments; take-aways above 100 sqm'},
-   {landUse:'B1', subLandUse:'business offices'},
-   {landUse:'B1', subLandUse:'light industry and research and development'},
-   {landUse:'B2-B8', subLandUse:'general industrial, storage or distribution'},
-   {landUse:'C1', subLandUse:'hotels'},
-   {landUse:'C2', subLandUse:'hospitals'},
-   {landUse:'C2', subLandUse:'care homes / secure accommodation'},
-   {landUse:'C3-C4', subLandUse:'dwellings (all)'},
-   {landUse:'D1', subLandUse:'nurseries'},
-   {landUse:'D1', subLandUse:'primary schools / secondary schools/ sixth form colleges'},
-   {landUse:'D1', subLandUse:'universities and colleges'},
-   {landUse:'D1', subLandUse:'health centre, including dentists'},
-   {landUse:'D1', subLandUse:'health centre, including dentists'},
-   {landUse:'D2', subLandUse:'sports (e.g. sports hall, swimming, gymnasium, etc.)'},
-   {landUse:'D2', subLandUse:'other (e.g. cinema, bingo, etc.)'},
-   {landUse:'Other', subLandUse:'Student accomodation'},
-   {landUse:'Other', subLandUse:'Older persons housing'},    
+const landUseDefinitions = [
+   {landUse:'A1', subLandUse:'food retail above 100sqm', quantumType:'sqm'},
+   {landUse:'A1', subLandUse:'non-food retail above 100sqm', quantumType:'sqm'},
+   {landUse:'A2-A5', subLandUse:'financial / professional services; cafes & restaurants; drinking establishments; take-aways above 100 sqm', quantumType:'sqm'},
+   {landUse:'B1', subLandUse:'business offices', quantumType:'sqm'},
+   {landUse:'B1', subLandUse:'light industry and research and development', quantumType:'sqm'},
+   {landUse:'B2-B8', subLandUse:'general industrial, storage or distribution', quantumType:'sqm'},
+   {landUse:'C1', subLandUse:'hotels', quantumType:'bedrooms'},
+   {landUse:'C2', subLandUse:'hospitals', quantumType:'FTE staff'},
+   {landUse:'C2', subLandUse:'care homes / secure accommodation', quantumType:'FTE staff'},
+   {landUse:'C3-C4', subLandUse:'dwellings (all)', quantumType:'dwellings'},
+   {landUse:'D1', subLandUse:'nurseries', quantumType:'FTE staff'},
+   {landUse:'D1', subLandUse:'primary schools / secondary schools/ sixth form colleges', quantumType:'FTE staff'},
+   {landUse:'D1', subLandUse:'universities and colleges', quantumType:'FTE staff'},
+   {landUse:'D1', subLandUse:'health centre, including dentists', quantumType:'FTE staff'},
+   {landUse:'D1', subLandUse:'health centre, including dentists', quantumType:'FTE staff'},
+   {landUse:'D2', subLandUse:'sports (e.g. sports hall, swimming, gymnasium, etc.)', quantumType:'FTE staff'},
+   {landUse:'D2', subLandUse:'other (e.g. cinema, bingo, etc.)', quantumType:'FTE staff'},
+   {landUse:'Other', subLandUse:'Student accomodation', quantumType:'bedrooms'},
+   {landUse:'Other', subLandUse:'Older persons housing', quantumType:'bedrooms'},    
 ]
 
 //create a display controller function
 const displayController = (() => {
     let landUseClassSel = document.getElementById('landUseClassSel');
-    let landUseSubClassSel = document.getElementById('landUseSubClassSel');
+    let subLandUseClassSel = document.getElementById('subLandUseClassSel');
     //create a function to populate and link the cascading dropdowns
     const fillDropdowns = () => {
-        let landUseArray = newLandUseDefinitions.map(object => object.landUse);
+        let landUseArray = landUseDefinitions.map(object => object.landUse);
         let uniqueLandUses = [...new Set(landUseArray)];
 
         for (let index = 0; index < uniqueLandUses.length; index++) {
@@ -60,20 +39,22 @@ const displayController = (() => {
     };
     
     const fillSubDropdowns = () => {
-        let newLandUseDefinitionsArray = newLandUseDefinitions.filter(function(e) {
-            return e.landUse === landUseClassSel.options[landUseClassSel.selectedIndex].text;;
+        subLandUseClassSel.innerText ='';
+        let landUseDefinitionsArray = landUseDefinitions.filter(function(e) {
+            return e.landUse === landUseClassSel.options[landUseClassSel.selectedIndex].text;
         })
-        let subLandUseArray = newLandUseDefinitionsArray.map(object => object.subLandUse);
+        let subLandUseArray = landUseDefinitionsArray.map(object => object.subLandUse);
 
         for (let index = 0; index < subLandUseArray.length; index++) {
             const subLandUseClass = subLandUseArray[index];
-            landUseSubClassSel.options[landUseSubClassSel.options.length] = new Option(subLandUseClass, subLandUseClass);
+            subLandUseClassSel.options[subLandUseClassSel.options.length] = new Option(subLandUseClass, subLandUseClass);
         };
+        fillCalculatorCells();
     };
 
     const fillCalculatorCells = () => {
         let landUseClassSelection = landUseClassSel.options[landUseClassSel.selectedIndex].text;
-        let landUseSubClassSelection = landUseSubClassSel.options[landUseSubClassSel.selectedIndex].text;
+        let subLandUseClassSelection = subLandUseClassSel.options[subLandUseClassSel.selectedIndex].text;
         let landUseClassCell = document.getElementById('landUseClassCell');
         let landUseSubClassCell = document.getElementById('landUseSubClassCell');
 
@@ -82,15 +63,16 @@ const displayController = (() => {
         } else {
             landUseClassCell.textContent = landUseClassSelection;
         };
-        if (landUseSubClassSelection == 'Select Sub-Class') {
+        if (subLandUseClassSelection == 'Select Sub-Class') {
             return;
         } else {
-            landUseSubClassCell.textContent = landUseSubClassSelection;
+            landUseSubClassCell.textContent = subLandUseClassSelection;
+            logicController.setQuantumType();
         };
     };
     
     landUseClassSel.addEventListener('change',fillSubDropdowns);
-    landUseSubClassSel.addEventListener('change',fillCalculatorCells);
+    subLandUseClassSel.addEventListener('change',fillCalculatorCells);
     
     return {   
         fillDropdowns,
@@ -103,3 +85,19 @@ displayController.fillDropdowns();
 displayController.fillCalculatorCells();
 
 //define logic controller for calculator
+const logicController = (() => {
+    
+    //create a function to populate and link the cascading dropdowns
+    const setQuantumType = () => {
+        let selectedSubLandUse = subLandUseClassSel.options[subLandUseClassSel.selectedIndex].text;
+        let quantumTypeElement = document.getElementById('quantumType');
+        let selectedObject = landUseDefinitions[landUseDefinitions.map(function (item) { return item.subLandUse; }).indexOf(selectedSubLandUse)];
+        let selectedQuantumType = selectedObject.quantumType;
+
+        quantumTypeElement.textContent = selectedQuantumType;
+    };
+    
+    return {   
+        setQuantumType,
+    };    
+})();
