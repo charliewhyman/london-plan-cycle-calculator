b101f76a43c46fe93a6dcac2ee2d0a62c987c5db
cwhyman <cwhyma@gmail.com>
2021-09-04 00:39:21 +0100

    improved dropdown functionality to reset form

--- js/script.js
+++ js/script.js
@@ -25,7 +25,10 @@ const landUseDefinitions = [
 
 const shortStayParkingStandards = [
     {subLandUse:'food retail above 100sqm', higherStandards:true, Boundary1:750, Ratio1:20, Boundary2:Infinity, Ratio2:150},
-    {subLandUse:'food retail above 100sqm', higherStandards:false, Boundary1:750, Ratio1:40, Boundary2:Infinity, Ratio2:300}
+    {subLandUse:'food retail above 100sqm', higherStandards:false, Boundary1:750, Ratio1:40, Boundary2:Infinity, Ratio2:300},
+
+    {subLandUse:'non-food retail above 100sqm', higherStandards:true, Boundary1:1000, Ratio1:60, Boundary2:Infinity, Ratio2:500},
+    {subLandUse:'non-food retail above 100sqm', higherStandards:false, Boundary1:1000, Ratio1:125, Boundary2:Infinity, Ratio2:1000},
  ]
 
 //create a display controller function
@@ -45,6 +48,7 @@ const displayController = (() => {
     
     const fillSubDropdowns = () => {
         let landUseClassSelection = landUseClassSel.options[landUseClassSel.selectedIndex].text;
+        
         if (landUseClassSelection == 'Select Class') {
             return;
         } else {
@@ -53,12 +57,22 @@ const displayController = (() => {
             })
             let subLandUseArray = landUseDefinitionsArray.map(object => object.subLandUse);
             subLandUseClassSel.options.length = 0;
+            subLandUseClassSel.options[subLandUseClassSel.options.length] = new Option('Select Sub-Class');
             for (let index = 0; index < subLandUseArray.length; index++) {
                 const subLandUseClass = subLandUseArray[index];
                 subLandUseClassSel.options[subLandUseClassSel.options.length] = new Option(subLandUseClass, subLandUseClass);
             };
         }
         
+    };
+
+    const resetForm = () => {
+        landUseClassSel.selectedIndex = 0;
+        subLandUseClassSel.selectedIndex = 0;
+        landUseClassCell.textContent = '';
+        landUseSubClassCell.textContent = '';
+        let quantumTypeElement = document.getElementById('quantumType');
+        quantumTypeElement.textContent = '';
     };
 
     const fillCalculatorCells = () => {
@@ -72,10 +86,14 @@ const displayController = (() => {
             landUseSubClassCell.textContent = '';
 
         } else if (landUseClassSelection === 'Select Class' && subLandUseClassSelection !== 'Select Sub-Class') {
+            console.log('test1')
+            resetForm();
             return;
 
         } else if (landUseClassSelection !== 'Select Class' && subLandUseClassSelection === 'Select Sub-Class') {
             subLandUseClassSel.selectedIndex = 0;
+            console.log('test2')
+
         } else {
             landUseClassCell.textContent = landUseClassSelection;
             landUseSubClassCell.textContent = subLandUseClassSelection;
@@ -84,13 +102,14 @@ const displayController = (() => {
         };
     };
 
-    landUseClassSel.addEventListener('change',fillCalculatorCells);
     landUseClassSel.addEventListener('change',fillSubDropdowns);
+    landUseClassSel.addEventListener('change',fillCalculatorCells);
     subLandUseClassSel.addEventListener('change',fillCalculatorCells);
     
     return {   
         fillDropdowns,
         fillSubDropdowns,
+        resetForm,
         fillCalculatorCells,
     };    
 })();
@@ -117,11 +136,12 @@ const logicController = (() => {
     const calculateShortStayParking = () => {
         let selectedSubLandUse = subLandUseClassSel.options[subLandUseClassSel.selectedIndex].text;
         let higherStandardsSelection = document.querySelector('input[name="choice-radio"]:checked').value;        ;
-
+        console.log(higherStandardsSelection)
         let roundedShortStayParking = 'Enter quantum';
-        let result = shortStayParkingStandards.find(x => x.subLandUse === selectedSubLandUse && x.higherStandards === false)
+        let parkingStandardsResult = shortStayParkingStandards.find(x => x.subLandUse === selectedSubLandUse && x.higherStandards == higherStandardsSelection)
 
-
+        //if statement to determine required number of parking spaces
+        
 
         return roundedShortStayParking;
 
@@ -129,13 +149,7 @@ const logicController = (() => {
 
     //calculate the required number of long-stay cycle parking spaces, based on the input quantum and selections
     const calculateLongStayParking = () => {
-        let roundedLongStayParking = 'Enter quantum';
-
-        //if statement to calculate required long-stay parking
-
         
-        return roundedLongStayParking;
-
     };
 
     const updateOutput = () => {
